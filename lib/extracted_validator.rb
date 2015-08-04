6bceb431348d63825fcc45d8464ec3d5523e7908
Sergey Gernyak <sergeg1990@gmail.com>
2015-08-04 10:43:21 +0300

    Basic concern skeleton. ref #1

--- lib/extracted_validator.rb
+++ lib/extracted_validator.rb
@@ -2,4 +2,8 @@ require "extracted_validator/version"
 
 module ExtractedValidator
   autoload :Base, 'extracted_validator/base'
+
+  module Concerns
+    autoload :AssociationsValidation, 'extracted_validator/concerns/associations_validation'
+  end
 end
---
+++ lib/extracted_validator/concerns/associations_validation.rb
@@ -0,0 +1,36 @@
+require 'active_support'
+require 'active_support/core_ext/module/aliasing'
+require 'active_support/core_ext/class/attribute_accessors'
+
+module ExtractedValidator
+  module Concerns
+    #
+    # Adds ability to validate associations.
+    #
+    module AssociationsValidation
+      extend ActiveSupport::Concern
+
+      included do
+        cattr_reader :assoc_validations_register
+        @@assoc_validations_register = []
+        alias_method_chain :valid?, :associations
+      end
+
+      module ClassMethods
+        #
+        # Adds association validation
+        #
+        # Parameters:
+        # name - associstion name
+        # *by* - class of validator
+        #
+        def validating(name, by: nil)
+          @@assoc_validations_register << name
+        end
+      end
+
+      def valid_with_associations?
+      end
+    end
+  end
+end
