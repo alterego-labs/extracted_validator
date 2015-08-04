995b1fc490d143f8120325f588a853c62d1b53bf
Sergey Gernyak <sergeg1990@gmail.com>
2015-08-04 09:26:32 +0300

    Some docs a bit

--- lib/extracted_validator/base.rb
+++ lib/extracted_validator/base.rb
@@ -3,6 +3,23 @@ require 'active_record'
 require 'active_support/core_ext/module/delegation'
 
 module ExtractedValidator
+  ##
+  # Creates custom, use-case-dependently validators.
+  # There are many cases when one model takes place in several use cases
+  # requiring different validation logic. You may stuff goal model
+  # with some conditions whether using this validation or not.
+  # Due to this you model will swell and eventually become ugly part of code.
+  #
+  # ==== Example
+  #
+  # Lets defining validator when user signing up:
+  # # class UserSignUpValidator < ExtractedValidator::Base[User]
+  # #   validates :first_name, presence: true
+  # #   validates :last_name, presence: true
+  # # end
+  #
+  # And now in your use-case class use it:
+  # # is_valid = UserSignUpValidator.new(new_user).valid?
   class Base < SimpleDelegator
     include ActiveRecord::Validations
 
